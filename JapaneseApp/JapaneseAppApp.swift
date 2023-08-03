2286eb40a17ba1a185585f3cc3b4737bc9babeb9
Alejandro Guzman <ketokunxd@gmail.com>
2023-08-03 17:22:06 -0500

    Initial Commit

---
+++ JapaneseApp/JapaneseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapaneseAppApp.swift
+//  JapaneseApp
+//
+//  Created by Alejandro Guzman Bolivar on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct JapaneseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
